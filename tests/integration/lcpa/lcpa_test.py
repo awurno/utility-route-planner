830bcb3a7e9a1d67e7dd0d80eaba827b60ae31e3
Jelmar <jelmar.versleijen@alliander.com>
2024-03-14 10:08:34 +0100

    Add test with larger area

--- tests/integration/lcpa/lcpa_test.py
+++ tests/integration/lcpa/lcpa_test.py
@@ -3,13 +3,13 @@ import shapely
 
 from src.models.lcpa.lcpa_main import get_lcpa_utility_route
 from settings import Config
-from src.util.write import delete_geopackage
+from src.util.write import reset_geopackage
 import geopandas as gpd
 
 
 @pytest.fixture
 def setup_monkeypatch_debug(monkeypatch):
-    delete_geopackage(Config.PATH_LCPA_GEOPACKAGE)
+    reset_geopackage(Config.PATH_LCPA_GEOPACKAGE)
     monkeypatch.setattr(Config, "DEBUG", True)
 
 
@@ -43,6 +43,17 @@ class TestUtilityRoutes:
             utility_route_sketch=shapely.LineString(
                 [(193077.740, 466510.697), (193043.338, 466490.707), (193055.374, 466489.049), (193031.551, 466474.721)]
             ),
+            project_area=project_area,
+        )
+
+        for route_point in lcpa_engine.route_model.route_points.geometry.tolist():
+            assert lcpa_engine.lcpa_result.dwithin(route_point, Config.RASTER_CELL_SIZE)
+
+    def test_get_utility_route_larger_area_with_project_area(self):
+        project_area = gpd.read_file(Config.PATH_PROJECT_AREA).iloc[0].geometry
+        lcpa_engine = get_lcpa_utility_route(
+            path_raster=Config.PATH_EXAMPLE_RASTER_1,
+            utility_route_sketch=shapely.LineString([(193077.740, 466510.697), (193383.28, 466452.02)]),
             project_area=project_area,
         )
 
