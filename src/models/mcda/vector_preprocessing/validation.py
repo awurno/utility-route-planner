4a1069895eaceefbeb73a6f6d5f1268bf7de4b7e
Jelmar <jelmar.versleijen@alliander.com>
2024-04-04 13:57:03 +0200

    Clarify validation

--- src/models/mcda/vector_preprocessing/validation.py
+++ src/models/mcda/vector_preprocessing/validation.py
@@ -5,5 +5,5 @@ def validate_values_to_reclassify(values_to_reclassify: list, assigned_values: d
     for value in values_to_reclassify:
         if not assigned_values.get(value):
             raise UnassignedValueFoundDuringReclassify(
-                f"Value: {value} is not present in the weight values dictionary: {assigned_values.keys}. Add it to the mcda_presets.py."
+                f"Value: {value} is not present in the weight values dictionary as valid non-zero integer: {assigned_values.keys()}. Add it to the mcda_presets.py."
             )
