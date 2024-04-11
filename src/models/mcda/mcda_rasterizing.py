6414b75688a7ded08c36663534a9682e4c419649
Jelmar <jelmar.versleijen@alliander.com>
2024-04-11 12:04:44 +0200

    Add blocksize to rasters, fix raster order per criteria

--- src/models/mcda/mcda_rasterizing.py
+++ src/models/mcda/mcda_rasterizing.py
@@ -4,6 +4,7 @@ import shapely
 import structlog
 import rasterio
 import rasterio.features
+import rasterio.merge
 import geopandas as gpd
 import affine
 
@@ -18,9 +19,12 @@ def rasterize_vector_data(
     criterion: str,
     project_area: shapely.MultiPolygon | shapely.Polygon,
     gdf_to_rasterize: gpd.GeoDataFrame,
-    cell_size: int,
+    cell_size: int | float,
 ) -> str:
-    """Burns the vector data to the project area in the desired raster cell size."""
+    """
+    Burns the vector data to the project area in the desired raster cell size.
+    If values overlap in the geodataframe, pick the highest value
+    """
     minx, miny, maxx, maxy = project_area.bounds
 
     # In order to fit the given cell size to the project area bounds, we slightly extend the maxx and maxy accordingly.
@@ -30,27 +34,33 @@ def rasterize_vector_data(
     width = math.ceil((maxx - minx) / cell_size)
     height = math.ceil((maxy - miny) / cell_size)
 
-    # the profile is used for creating the GeoTiff. It can be copied from an existing raster or manually defined.
     # noinspection PyUnresolvedReferences
+    nodata = 0
     profile = {
         "driver": "GTiff",
         "dtype": "int16",
-        "nodata": 0,  # TODO replace
+        "nodata": nodata,
+        "compress": "lzw",
+        "tiled": True,
         "width": width,
         "height": height,
+        "blockxsize": Config.RASTER_BLOCK_SIZE,
+        "blockysize": Config.RASTER_BLOCK_SIZE,
         "count": 1,
-        "crs": rasterio.crs.CRS({"init": "epsg:28992"}),
+        "crs": rasterio.CRS.from_epsg(Config.CRS),  # rasterio.crs.CRS({"init": "epsg:28992"})
         "transform": affine.Affine(cell_size, 0.0, round(minx), 0.0, -cell_size, round(maxy)),
     }
 
     logger.info(f"Rasterizing layer: {criterion} in cell size: {cell_size} meters")
     # TODO check if we can use /vsimem/
+    gdf_to_rasterize.sort_values("suitability_value", ascending=True, inplace=True)
     path_raster = Config.PATH_RESULTS / f"{raster_prefix+criterion}.tif"
     with rasterio.open(path_raster, "w+", **profile) as out:
         out_arr = out.read(1)
-        # TODO check if sorting the order here impacts the raster creation, order by suitability_value
         shapes = ((geom, value) for geom, value in zip(gdf_to_rasterize.geometry, gdf_to_rasterize.suitability_value))
-        burned = rasterio.features.rasterize(shapes=shapes, fill=0, out=out_arr, transform=out.transform)
+        burned = rasterio.features.rasterize(
+            shapes=shapes, fill=nodata, out=out_arr, transform=out.transform, all_touched=False
+        )
         out.write_band(1, burned)
 
     return path_raster.__str__()
@@ -58,13 +68,17 @@ def rasterize_vector_data(
 
 def sum_rasters(rasters_to_sum: list[dict]):
     logger.info(f"Starting summing {len(rasters_to_sum)} rasters into the final cost surface.")
+    # TODO use groups with summing
 
     src_files_to_mosaic = [rasterio.open(list(i.keys())[0]) for i in rasters_to_sum]
     # Merge the rasters
+    # TODO check the merge docs
     mosaic, out_trans = rasterio.merge.merge(src_files_to_mosaic)
+    # TODO set upper/lower limit of raster
     summed_raster = mosaic.sum(axis=0)
 
     # Update metadata
+    # TODO set proper datatype
     out_meta = src_files_to_mosaic[0].meta.copy()
     out_meta.update(
         {"driver": "GTiff", "height": summed_raster.shape[0], "width": summed_raster.shape[1], "transform": out_trans}
--- tests/integration/mcda/mcda_raster_test.py
+++ tests/integration/mcda/mcda_raster_test.py
@@ -1,5 +1,9 @@
 import pytest
 import geopandas as gpd
+import rasterio
+import rasterio.sample
+import shapely
+import numpy as np
 
 from settings import Config
 from src.models.mcda.exceptions import RasterCellSizeTooSmall
@@ -10,3 +14,46 @@ def test_rasterize_vector_data_cell_size_error():
     with pytest.raises(RasterCellSizeTooSmall):
         project_area = gpd.read_file(Config.PATH_PROJECT_AREA_EDE_COMPONISTENBUURT).iloc[0].geometry
         rasterize_vector_data("temp", project_area, gpd.GeoDataFrame(), 500000)
+
+
+def test_rasterize_different_order():
+    gdf = gpd.GeoDataFrame(
+        data=[
+            # These layers all overlap each other.
+            [1, shapely.Polygon([[174872, 451093], [174870, 451082], [174876, 451081], [174872, 451093]])],
+            [10, shapely.Polygon([[174872, 451093], [174870, 451082], [174876, 451081], [174872, 451093]])],
+            [10, shapely.Polygon([[174872, 451093], [174870, 451082], [174876, 451081], [174872, 451093]])],
+            [1, shapely.Polygon([[174872, 451093], [174870, 451082], [174876, 451081], [174872, 451093]])],
+            # One larger partly overlapping polygon with a unique value.
+            [5, shapely.Polygon([[174872, 451093], [174870, 451082], [174876, 451081], [174872, 451093]]).buffer(50)],
+        ],
+        geometry="geometry",
+        crs=Config.CRS,
+        columns=["suitability_value", "geometry"],
+    )
+    sort_asc = gdf.sort_values("suitability_value", ascending=True)
+    sort_desc = gdf.sort_values("suitability_value", ascending=False)
+
+    gdfs_to_rasterize = [gdf, sort_desc, sort_asc]
+    for gdf in gdfs_to_rasterize:
+        rasterized_gdf = rasterize_vector_data(
+            "pytest_",
+            "test_rasterize",
+            gpd.read_file(Config.PATH_PROJECT_AREA_EDE_COMPONISTENBUURT).iloc[0].geometry,
+            gdf,
+            0.5,
+        )
+        with rasterio.open(rasterized_gdf, "r") as out:
+            result = out.read(1)
+            unique_values = np.unique(result)
+            assert set(unique_values) == set([0, 5, 10])
+            # Check that the overlapping part has the highest value
+            values = list(
+                rasterio.sample.sample_gen(
+                    out, [[174872.396, 451084.460], [174868.573, 451086.020], [174985.83, 451101.57], [0, 0]]
+                )
+            )
+            assert values[0][0] == 10  # middle of the overlapping part
+            assert values[1][0] == 5  # just next to the polygon
+            assert values[2][0] == 0  # outside the polygons
+            assert values[3][0] == 0  # outside the project area
