bc82af6a804e84164e56d760d45e9daf85ff6c04
Jelmar Versleijen <jelmar.versleijen@alliander.com>
2024-06-13 10:03:10 +0200

    Add some characters for visibility in the log
    Signed-off-by: Jelmar Versleijen <jelmar.versleijen@alliander.com>

--- utility_route_planner/util/timer.py
+++ utility_route_planner/util/timer.py
@@ -9,7 +9,7 @@ def time_function(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        logger.info(f"Function {func.__name__} took {end_time - start_time} to execute.")
+        logger.info(f"*** Function {func.__name__} took {end_time - start_time} to execute.")
         return result
 
     return wrapper
