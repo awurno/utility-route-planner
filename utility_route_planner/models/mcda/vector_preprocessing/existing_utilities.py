4b46ba411ab87250e03ea156b04d35e5d6cd06eb
Jelmar Versleijen <jelmar.versleijen@alliander.com>
2024-11-14 17:27:59 +0100

    Add some notes for possible unwanted overlap in existing utilities.
    Signed-off-by: Jelmar Versleijen <jelmar.versleijen@alliander.com>

--- utility_route_planner/models/mcda/vector_preprocessing/existing_utilities.py
+++ utility_route_planner/models/mcda/vector_preprocessing/existing_utilities.py
@@ -21,14 +21,14 @@ class ExistingUtilities(VectorPreprocessorBase):
     def specific_preprocess(
         self, input_gdf: list[gpd.GeoDataFrame], criterion: RasterPresetCriteria
     ) -> gpd.GeoDataFrame:
-        input_gdf = self._set_suitability_values(input_gdf, criterion.weight_values, criterion.geometry_values)  # type: ignore
+        input_gdf = self._set_suitability_and_geometry_values(input_gdf, criterion.weight_values, criterion.geometry_values)  # type: ignore
         return input_gdf
 
     @staticmethod
-    def _set_suitability_values(
+    def _set_suitability_and_geometry_values(
         input_gdf: list[gpd.GeoDataFrame], weight_values: dict, buffer_values: dict
     ) -> gpd.GeoDataFrame:
-        logger.info("Setting suitability values.")
+        logger.info("Setting suitability and updating geometry values.")
         # High voltage assets
         gdf_high_voltage_underground, gdf_high_voltage_overhead, gdf_gasunie_leiding = (
             get_empty_geodataframe(),
@@ -44,12 +44,14 @@ class ExistingUtilities(VectorPreprocessorBase):
                     gdf_high_voltage_overhead["geometry"] = gdf_high_voltage_overhead["geometry"].buffer(
                         buffer_values["hoogspanning_bovengronds_buffer"]
                     )
+                    # Possibly we may need to dissolve based on highest suitability value.
                 elif gdf.iloc[0].type == "high_voltage_cable_underground":
                     gdf_high_voltage_underground = gdf.copy()
                     gdf_high_voltage_underground["suitability_value"] = weight_values["hoogspanning_ondergronds"]
                     gdf_high_voltage_underground["geometry"] = gdf_high_voltage_underground["geometry"].buffer(
                         buffer_values["hoogspanning_ondergronds_buffer"]
                     )
+                    # Possibly we may need to dissolve based on highest suitability value.
             elif "Leiding" in gdf.columns:
                 gdf_gasunie_leiding = gdf.copy()
                 gdf_gasunie_leiding = gdf_gasunie_leiding[gdf_gasunie_leiding["StatusOperationeel"] == "In Bedrijf"]
@@ -57,6 +59,7 @@ class ExistingUtilities(VectorPreprocessorBase):
                 gdf_gasunie_leiding["geometry"] = gdf_gasunie_leiding["geometry"].buffer(
                     buffer_values["gasunie_leidingen_buffer"]
                 )
+                gdf_gasunie_leiding = gdf_gasunie_leiding.dissolve()
             else:
                 logger.warning(f"Unknown layer found in existing utilities: {gdf.columns}.")
 
