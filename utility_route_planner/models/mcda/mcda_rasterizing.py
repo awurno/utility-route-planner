Use block bounding boxes instead of grid bounding boxes to ensure correct offset calculation
Signed-off-by: Djesse Dirckx <djesse.dirckx@alliander.com>

utility_route_planner/models/mcda/mcda_rasterizing.py
@@ -168,10 +168,13 @@ def process_raster_groups(
     return processed_raster
 
 
-def write_raster_block(complete_raster: np.ma.array, raster_settings: McdaRasterSettings, final_raster_name) -> str:
+def write_raster_block(
+    complete_raster: np.ma.array, raster_settings: McdaRasterSettings, final_raster_name
+) -> tuple[str, list[float]]:
     raster_settings.nodata = Config.FINAL_RASTER_NO_DATA
     final_raster_path = Config.PATH_RESULTS / (final_raster_name + ".tif")
     with rasterio.open(final_raster_path, "w", **asdict(raster_settings)) as dest:
         dest.write(np.ma.filled(complete_raster, Config.FINAL_RASTER_NO_DATA), 1)
+        bbox = list(dest.bounds)
 
-    return final_raster_path.__str__()
+    return final_raster_path.__str__(), bbox

utility_route_planner/models/mcda/vrt_builder.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import xml.etree.cElementTree as et
 from xml.dom import minidom
 
+import numpy as np
 import rasterio
 from pyproj import CRS
 from rasterio.enums import ColorInterp
@@ -13,17 +14,17 @@ class VRTBuilder:
     def __init__(
         self,
         block_files: list[str],
+        block_bboxes: list[float],
         crs: CRS,
         resolution,
-        raster_bounds: list[float],
         vrt_path: Path,
     ):
         self.block_files = block_files
         self.crs = crs
         self.resolution = resolution
-        self.min_x, self.min_y, self.max_x, self.max_y = raster_bounds
         self.vrt_path = vrt_path
         self.xml_datatype = "Int8"
+        self.min_x, self.min_y, self.max_x, self.max_y = np.array(block_bboxes).min(axis=0)
 
     def build_and_write_to_disk(self):
         vrt_tree, raster_band = self.setup_tree()