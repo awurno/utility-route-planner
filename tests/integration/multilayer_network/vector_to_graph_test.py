a79f111117195111b7d5bb68d77ecc60d435d5e2
Djesse Dirckx <djesse.dirckx@alliander.com>
2025-05-15 16:17:31 +0200

    Added function to convert rustworkx graph to geopandas dataframes (nodes and edges) Signed-off-by: Djesse Dirckx <djesse.dirckx@alliander.com>

--- tests/integration/multilayer_network/vector_to_graph_test.py
+++ tests/integration/multilayer_network/vector_to_graph_test.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import osmnx as ox
 import pandas as pd
 import pytest
 import shapely
@@ -10,6 +9,7 @@ import geopandas as gpd
 
 from models.mcda.mcda_engine import McdaCostSurfaceEngine
 from models.multilayer_network.hexagon_graph.hexagon_graph_builder import HexagonGraphBuilder
+from models.multilayer_network.hexagon_graph.hexagon_utils import convert_hexagon_graph_to_gdfs
 from settings import Config
 from util.write import write_results_to_geopackage
 
@@ -48,25 +48,26 @@ class TestVectorToGraph:
         )
 
     @pytest.fixture()
-    def vectors_for_project_areas(self, larger_project_area: shapely.Polygon) -> gpd.GeoDataFrame:
+    def vectors_for_project_areas(self, ede_project_area: shapely.Polygon) -> gpd.GeoDataFrame:
         mcda_engine = McdaCostSurfaceEngine(
             Config.RASTER_PRESET_NAME_BENCHMARK,
             Config.PYTEST_PATH_GEOPACKAGE_MCDA,
-            larger_project_area,
+            ede_project_area,
         )
         mcda_engine.preprocess_vectors()
         concatenated_vectors = pd.concat(mcda_engine.processed_vectors.values())
         concatenated_vectors = concatenated_vectors.reset_index(drop=True)
         return gpd.GeoDataFrame(concatenated_vectors)
 
-    def test_vector_to_graph(self, vectors_for_project_areas: gpd.GeoDataFrame):
+    def test_vector_to_graph(self, vectors_for_project_areas: gpd.GeoDataFrame, debug: bool = True):
         hexagon_graph_builder = HexagonGraphBuilder(vectors_for_project_areas, hexagon_size=0.5)
         graph = hexagon_graph_builder.build_graph()
 
-        nodes_gdf, edges_gdf = ox.convert.graph_to_gdfs(graph)
-        write_results_to_geopackage(
-            Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, nodes_gdf, "graph_nodes", overwrite=True
-        )
-        write_results_to_geopackage(
-            Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, edges_gdf, "graph_edges", overwrite=True
-        )
+        if debug:
+            nodes_gdf, edges_gdf = convert_hexagon_graph_to_gdfs(graph)
+            write_results_to_geopackage(
+                Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, nodes_gdf, "graph_nodes", overwrite=True
+            )
+            write_results_to_geopackage(
+                Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, edges_gdf, "graph_edges", overwrite=True
+            )
--- utility_route_planner/models/multilayer_network/hexagon_graph/hexagon_utils.py
+++ utility_route_planner/models/multilayer_network/hexagon_graph/hexagon_utils.py
@@ -4,9 +4,37 @@
 
 import math
 
+import geopandas as gpd
+import numpy as np
+import rustworkx as rx
+import shapely
+
+from settings import Config
+from util.timer import time_function
+
 
 def get_hexagon_width_and_height(hexagon_size: float) -> tuple[float, float]:
     hexagon_width = 2 * hexagon_size
     hexagon_height = math.sqrt(3) * hexagon_size
 
     return hexagon_width, hexagon_height
+
+
+@time_function
+def convert_hexagon_graph_to_gdfs(hexagon_graph: rx.PyGraph) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
+    nodes = hexagon_graph.nodes()
+
+    nodes_gdf = gpd.GeoDataFrame.from_dict(dict(nodes), orient="index")
+    nodes_gdf = nodes_gdf.set_geometry(gpd.points_from_xy(nodes_gdf["x"], nodes_gdf["y"], crs=Config.CRS))
+
+    edges_gdf = gpd.GeoDataFrame(hexagon_graph.weighted_edge_list(), columns=["u", "v", "weight"])
+    u_coords = nodes_gdf.loc[edges_gdf["u"], ["x", "y"]].values
+    v_coords = nodes_gdf.loc[edges_gdf["v"], ["x", "y"]].values
+
+    # Stack u and v coordinates on axis 1 to get correct linestring coordinate format: [[u_x, u_y], [v_x, v_y]]
+    line_string_coords = np.stack([u_coords, v_coords], axis=1)
+    edge_line_strings = shapely.linestrings(line_string_coords)
+
+    edges_gdf = edges_gdf.set_geometry(edge_line_strings, crs=Config.CRS)
+
+    return nodes_gdf, edges_gdf
