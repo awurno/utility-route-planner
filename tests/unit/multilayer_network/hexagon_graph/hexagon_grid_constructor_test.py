b2b157781ae4d9fbdf958157a7eaf8326a88804b
Djesse Dirckx <djesse.dirckx@alliander.com>
2025-08-07 13:44:13 +0200

    Improved comments for square project area hexagon construction test
    Signed-off-by: Djesse Dirckx <djesse.dirckx@alliander.com>

--- tests/unit/multilayer_network/hexagon_graph/hexagon_grid_constructor_test.py
+++ tests/unit/multilayer_network/hexagon_graph/hexagon_grid_constructor_test.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import math
 import geopandas as gpd
 import numpy as np
 import pytest
@@ -63,23 +64,33 @@ class TestHexagonalGridConstructor:
         self, grid_constructor: HexagonalGridConstructor, square_project_area: shapely.Polygon
     ):
         result = grid_constructor.construct_hexagonal_grid_for_bounding_box(square_project_area)
-        hexagon_width, hexagon_height = 0.75, 0.866
 
-        # Test spacing between points (based on equation?)
         coordinates = result.geometry.get_coordinates()
         x_coordinates = coordinates["x"].values.reshape(6, 7)
         y_coordinates = coordinates["y"].values.reshape(6, 7)
 
+        # Set expected spacing between hexagon points based on known equations.
+        expected_horizontal_spacing = 3 / 2 * grid_constructor.hexagon_size
+        expected_vertical_spacing = math.sqrt(3) * grid_constructor.hexagon_size
+
         # Verify that spacing between x- and y-coordinates satisfy are equal to hexagon heigth and width
         # and do therefore meet hexagon constaints.
+
+        # Verify that spacing between x-coordinates of hexagon center points equals the expected horizontal spacing
         x_spacing = np.diff(x_coordinates, axis=1)
-        assert all(x_space == pytest.approx(hexagon_width, abs=1e-4) for x_space in x_spacing)
+        assert all(x_space == pytest.approx(expected_horizontal_spacing, abs=1e-4) for x_space in x_spacing)
 
+        # Verify that spacing between y-coordinates of hexagon center points equals the expected vertical spacing
         y_vertical_spacing = np.diff(y_coordinates, axis=0)
-        assert all(y_space == pytest.approx(hexagon_height, abs=1e-4) for y_space in y_vertical_spacing)
+        assert all(y_space == pytest.approx(expected_vertical_spacing, abs=1e-4) for y_space in y_vertical_spacing)
 
+        # Verify that y-coordinates of horizontally neighbouring hexagon center points are 1/2 of the expected vertical spacing
+        # separated from each other. This is due to alternating offset of neighbouring hexagon points.
         y_horizontal_spacing = np.diff(y_coordinates, axis=1)
-        assert all(y_space == pytest.approx(hexagon_height / 2, abs=1e-4) for y_space in np.abs(y_horizontal_spacing))
+        assert all(
+            y_space == pytest.approx(expected_vertical_spacing / 2, abs=1e-4)
+            for y_space in np.abs(y_horizontal_spacing)
+        )
 
         # As every even column is offset by 1/2 the hexagon height, the horizontal spacing should always be negative. For
         # all odd columns, the sign should be positive
