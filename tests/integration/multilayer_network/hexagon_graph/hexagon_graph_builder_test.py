80c4786af49e6ea226049787e0a43c0118a0eefd
Djesse Dirckx <djesse.dirckx@alliander.com>
2025-09-11 17:11:51 +0200

    Restructured integration test for hexagon grids
    Signed-off-by: Djesse Dirckx <djesse.dirckx@alliander.com>

--- tests/integration/multilayer_network/hexagon_graph/hexagon_graph_builder_test.py
+++ tests/integration/multilayer_network/hexagon_graph/hexagon_graph_builder_test.py
@@ -13,137 +13,151 @@ from utility_route_planner.models.multilayer_network.hexagon_graph.hexagon_utils
 from utility_route_planner.util.write import reset_geopackage, write_results_to_geopackage
 
 
-@pytest.fixture()
-def ede_project_area() -> shapely.MultiPolygon:
-    return (
-        gpd.read_file(Config.PYTEST_PATH_GEOPACKAGE_MCDA, layer=Config.PYTEST_LAYER_NAME_PROJECT_AREA).iloc[0].geometry
-    )
+class TestHexagonGraphBuilder:
+    """
+    This integration test tests whether artificially created vectors within a predefined project area are properly reflected
+    in the hexagonal grid. First, the hexagonal grid for a single criterion is tested. Next, multiple criteria are used
+    as input which enables more advanced testing with overlapping criteria.
+    """
 
+    @pytest.fixture()
+    def ede_project_area(self) -> shapely.MultiPolygon:
+        return (
+            gpd.read_file(Config.PYTEST_PATH_GEOPACKAGE_MCDA, layer=Config.PYTEST_LAYER_NAME_PROJECT_AREA)
+            .iloc[0]
+            .geometry
+        )
 
-def test_build_graph_for_single_criterion(
-    single_criterion_vectors: Callable,
-    ede_project_area: shapely.MultiPolygon,
-    debug: bool = False,
-):
-    max_value = Config.MAX_NODE_SUITABILITY_VALUE
-    min_value = Config.MIN_NODE_SUITABILITY_VALUE
-    single_criterion_vectors = single_criterion_vectors(max_value, min_value, max_value)
+    def test_build_graph_for_single_criterion(
+        self,
+        single_criterion_vectors: Callable,
+        ede_project_area: shapely.MultiPolygon,
+        debug: bool = False,
+    ):
+        max_value = Config.MAX_NODE_SUITABILITY_VALUE
+        min_value = Config.MIN_NODE_SUITABILITY_VALUE
+        single_criterion_vectors = single_criterion_vectors(max_value, min_value, max_value)
 
-    # Create a simple vector dict for the single criterion.
-    vectors = {"test": single_criterion_vectors}
-    raster_criteria_groups = {"test": "a"}
+        # Create a simple vector dict for the single criterion.
+        preprocessed_vectors = {"test": single_criterion_vectors}
+        raster_criteria_groups = {"test": "a"}
 
-    hexagon_graph_builder = HexagonGraphBuilder(
-        ede_project_area,
-        raster_criteria_groups,
-        vectors,
-        hexagon_size=0.5,
-    )
-    graph = hexagon_graph_builder.build_graph()
-    nodes_gdf, edges_gdf = convert_hexagon_graph_to_gdfs(graph)
+        hexagon_graph_builder = HexagonGraphBuilder(
+            ede_project_area,
+            raster_criteria_groups,
+            preprocessed_vectors,
+            hexagon_size=0.5,
+        )
+        graph = hexagon_graph_builder.build_graph()
+        nodes_gdf, edges_gdf = convert_hexagon_graph_to_gdfs(graph)
 
-    points_to_sample = gpd.GeoDataFrame(
-        data=[
-            # Multiple overlapping values, take the max value
-            [1, 10, shapely.Point(174871.877, 451084.402)],
-            # Single vector, must be equal to vector suitability value
-            [2, 5, shapely.Point(174868.877, 451086.134)],
-            # Vector value exceeds max node value, must be reset to max value
-            [3, max_value, shapely.Point(175012.877, 450908.599)],
-            # Vector value lower than node min value, must be reset to min value
-            [4, min_value, shapely.Point(175093.877, 450912.929)],
-            # Vector value equal to max value, must remain the same
-            [5, max_value, shapely.Point(174923.627, 450959.261)],
-        ],
-        geometry="geometry",
-        crs=Config.CRS,
-        columns=["sample_id", "expected_suitability_value", "geometry"],
-    )
+        sample_points = gpd.GeoDataFrame(
+            data=[
+                # Multiple overlapping values, take the max value
+                [1, 10, shapely.Point(174871.877, 451084.402)],
+                # Single vector, must be equal to vector suitability value
+                [2, 5, shapely.Point(174868.877, 451086.134)],
+                # Vector value exceeds max node value, must be reset to max value
+                [3, max_value, shapely.Point(175012.877, 450908.599)],
+                # Vector value lower than node min value, must be reset to min value
+                [4, min_value, shapely.Point(175093.877, 450912.929)],
+                # Vector value equal to max value, must remain the same
+                [5, max_value, shapely.Point(174923.627, 450959.261)],
+            ],
+            geometry="geometry",
+            crs=Config.CRS,
+            columns=["sample_id", "expected_suitability_value", "geometry"],
+        )
 
-    # Verify that the nodes near the sample points are equal to the expected value on the sample points.
-    joined_sample_points = points_to_sample.sjoin_nearest(nodes_gdf)
-    assert joined_sample_points["expected_suitability_value"].equals(joined_sample_points["suitability_value"])
+        # Verify that the nodes near the sample points are equal to the expected value on the sample points.
+        joined_sample_points = sample_points.sjoin_nearest(nodes_gdf)
+        assert joined_sample_points["expected_suitability_value"].equals(joined_sample_points["suitability_value"])
 
-    if debug:
+        if debug:
+            self.write_debug_output(ede_project_area, preprocessed_vectors, nodes_gdf, edges_gdf, sample_points)
+
+    def test_build_graph_for_multiple_criteria(
+        self, multi_criteria_vectors: Callable, ede_project_area: shapely.MultiPolygon, debug: bool = False
+    ):
+        max_value = Config.MAX_NODE_SUITABILITY_VALUE
+        min_value = Config.MIN_NODE_SUITABILITY_VALUE
+        multiple_criteria_vectors = multi_criteria_vectors(max_value, min_value)
+
+        raster_criteria_groups = {criterion_name: group for criterion_name, group, _ in multiple_criteria_vectors}
+        preprocessed_vectors = {
+            criterion_name: criterion_gdf for criterion_name, _, criterion_gdf in multiple_criteria_vectors
+        }
+
+        hexagon_graph_builder = HexagonGraphBuilder(
+            ede_project_area,
+            raster_criteria_groups,
+            preprocessed_vectors,
+            hexagon_size=0.5,
+        )
+
+        graph = hexagon_graph_builder.build_graph()
+        nodes_gdf, edges_gdf = convert_hexagon_graph_to_gdfs(graph)
+
+        sample_points = gpd.GeoDataFrame(
+            data=[
+                # Overlap between b1 and b2
+                [1, 14.0, shapely.Point(175090.35, 450911.67)],
+                # Overlap between a1, b1 and b2
+                [2, min_value, shapely.Point(175091.8234, 450911.7488)],
+                # Only b1
+                [3, -1.0, shapely.Point(175088.2180, 450912.7950)],
+                # Overlap between b1 and a1
+                [4, max_value, shapely.Point(175013.3110, 450910.3013)],
+                # Just a1
+                [5, 5.0, shapely.Point(174839.089, 451050.785)],
+                # Overlap between b1 and a1
+                [6, 70.0, shapely.Point(174813.2646, 451113.9146)],
+                # B1 and a1 sum is 0 here
+                [7, 0.0, shapely.Point(174833.90, 451067.57)],
+                # C1 overlaps a1
+                [8, max_value, shapely.Point(174878.65, 451132.89)],
+                # C1
+                [9, max_value, shapely.Point(174799.54, 451170.54)],
+                # C1 overlapping c1
+                [10, max_value, shapely.Point(174921.44, 451123.59)],
+                # C1 outside the project area
+                [11, max_value, shapely.Point(174745.32, 451159.41)],
+                # C2 overlapping b2
+                [12, max_value, shapely.Point(175092.267, 450908.932)],
+                # C2 overlapping b2, a1
+                [13, max_value, shapely.Point(175097.673, 450912.390)],
+                # C2 overlapping c1
+                [14, max_value, shapely.Point(174847.32, 451177.96)],
+            ],
+            geometry="geometry",
+            crs=Config.CRS,
+            columns=["sample_id", "expected_suitability_value", "geometry"],
+        )
+
+        # Verify that the nodes near the sample points are equal to the expected value on the sample points.
+        joined_sample_points = sample_points.sjoin_nearest(nodes_gdf)
+        assert joined_sample_points["expected_suitability_value"].equals(joined_sample_points["suitability_value"])
+
+        if debug:
+            self.write_debug_output(ede_project_area, preprocessed_vectors, nodes_gdf, edges_gdf, sample_points)
+
+    @staticmethod
+    def write_debug_output(
+        project_area: shapely.MultiPolygon,
+        criteria_vectors: dict[str, gpd.GeoDataFrame],
+        nodes_gdf: gpd.GeoDataFrame,
+        edges_gdf: gpd.GeoDataFrame,
+        sample_points: gpd.GeoDataFrame,
+    ):
         reset_geopackage(Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT)
         write_results_to_geopackage(
-            Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, ede_project_area, "pytest_project_area", overwrite=True
+            Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, project_area, "pytest_project_area", overwrite=True
         )
-        write_results_to_geopackage(
-            Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, single_criterion_vectors, "pytest_vectors", overwrite=True
-        )
-        write_results_to_geopackage(
-            Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, nodes_gdf, "pytest_graph_nodes", overwrite=True
-        )
-        write_results_to_geopackage(
-            Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, edges_gdf, "pytest_graph_edges", overwrite=True
-        )
-        write_results_to_geopackage(
-            Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, points_to_sample, "pytest_points_to_sample", overwrite=True
-        )
-
-
-def test_build_graph_for_multiple_criteria(
-    multi_criteria_vectors: gpd.GeoDataFrame, ede_project_area: shapely.MultiPolygon, debug: bool = False
-):
-    max_value = Config.MAX_NODE_SUITABILITY_VALUE
-    min_value = Config.MIN_NODE_SUITABILITY_VALUE
-    multiple_criteria_vectors = multi_criteria_vectors(max_value, min_value)
-
-    raster_criteria_groups = {criterion_name: group for criterion_name, group, _ in multiple_criteria_vectors}
-    preprocessed_vectors = {
-        criterion_name: criterion_gdf for criterion_name, _, criterion_gdf in multiple_criteria_vectors
-    }
-
-    hexagon_graph_builder = HexagonGraphBuilder(
-        ede_project_area,
-        raster_criteria_groups,
-        preprocessed_vectors,
-        hexagon_size=0.5,
-    )
-    hexagon_graph_builder.build_graph()
-
-    graph = hexagon_graph_builder.build_graph()
-    nodes_gdf, edges_gdf = convert_hexagon_graph_to_gdfs(graph)
-
-    points_to_sample = gpd.GeoDataFrame(
-        data=[
-            [1, 14.0, shapely.Point(175090.35, 450911.67)],  # overlap between b1 and b2
-            [2, min_value, shapely.Point(175091.8234, 450911.7488)],  # overlap between a1, b1 and b2
-            [3, -1.0, shapely.Point(175088.2180, 450912.7950)],  # only b1
-            [5, max_value, shapely.Point(175013.3110, 450910.3013)],  # overlap between b1 and a1
-            [6, 5.0, shapely.Point(174839.089, 451050.785)],  # just a1
-            [7, 70.0, shapely.Point(174813.2646, 451113.9146)],  # overlap between b1 and a1
-            [9, 0.0, shapely.Point(174833.90, 451067.57)],  # b1 and a1 sum is 0 here
-            [10, max_value, shapely.Point(174878.65, 451132.89)],  # c1 overlaps a1
-            [11, max_value, shapely.Point(174799.54, 451170.54)],  # c1
-            [12, max_value, shapely.Point(174921.44, 451123.59)],  # c1 overlapping c1
-            [13, max_value, shapely.Point(174745.32, 451159.41)],  # c1 outside the project area
-            [14, max_value, shapely.Point(175092.267, 450908.932)],  # c2 overlapping b2
-            [15, max_value, shapely.Point(175097.673, 450912.390)],  # c2 overlapping b2, a1
-            [16, max_value, shapely.Point(174847.32, 451177.96)],  # c2 overlapping c1
-        ],
-        geometry="geometry",
-        crs=Config.CRS,
-        columns=["sample_id", "expected_suitability_value", "geometry"],
-    )
-
-    # Verify that the nodes near the sample points are equal to the expected value on the sample points.
-    joined_sample_points = points_to_sample.sjoin_nearest(nodes_gdf)
-    assert joined_sample_points["expected_suitability_value"].equals(joined_sample_points["suitability_value"])
-
-    if debug:
-        reset_geopackage(Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT)
-        write_results_to_geopackage(
-            Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, ede_project_area, "pytest_project_area", overwrite=True
-        )
-        for criterion in multiple_criteria_vectors:
-            criterion_name, _, criterion_gdf = criterion
-
+        for name, gdf in criteria_vectors.items():
             write_results_to_geopackage(
                 Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT,
-                criterion_gdf,
-                f"pytest_sum_{criterion_name}",
+                gdf,
+                f"pytest_vector_{name}",
                 overwrite=True,
             )
 
@@ -154,5 +168,5 @@ def test_build_graph_for_multiple_criteria(
             Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, edges_gdf, "pytest_graph_edges", overwrite=True
         )
         write_results_to_geopackage(
-            Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, points_to_sample, "pytest_points_to_sample", overwrite=True
+            Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, sample_points, "pytest_points_to_sample", overwrite=True
         )
