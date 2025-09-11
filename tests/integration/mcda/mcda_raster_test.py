50caa0694bcd3302915e9407a3724ceb30d491e6
Djesse Dirckx <djesse.dirckx@alliander.com>
2025-09-11 10:41:22 +0200

    Create fixture for multiple criteria vectors
    Signed-off-by: Djesse Dirckx <djesse.dirckx@alliander.com>

--- tests/integration/mcda/mcda_raster_test.py
+++ tests/integration/mcda/mcda_raster_test.py
@@ -192,94 +192,12 @@ def test_rasterize_single_criterion(single_criterion_vectors: gpd.GeoDataFrame, 
             assert rasterized_vector[int(row_index)][int(col_index)] == row.expected_suitability_value
 
 
-def test_sum_rasters(debug=False):
+def test_sum_rasters(multi_criteria_vectors, debug: bool = False):
     max_value = Config.FINAL_RASTER_VALUE_LIMIT_UPPER
     min_value = Config.FINAL_RASTER_VALUE_LIMIT_LOWER
     no_data = Config.FINAL_RASTER_NO_DATA
-    # 4 rasters:
-    # 1. group a - partial overlap
-    criterion_a_1 = gpd.GeoDataFrame(
-        data=[
-            # These layers all overlap each other.
-            [1, shapely.Polygon([[174872, 451093], [174870, 451082], [174876, 451081], [174872, 451093]])],
-            [10, shapely.Polygon([[174872, 451093], [174870, 451082], [174876, 451081], [174872, 451093]])],
-            # One larger partly overlapping polygon with a unique value.
-            [5, shapely.Polygon([[174872, 451093], [174870, 451082], [174876, 451081], [174872, 451093]]).buffer(50)],
-            # These values should be reset to the min/max of the intermediate raster values
-            [
-                min_value - 1000,
-                shapely.Polygon([[175091, 450919], [175091, 450911], [175105, 450911], [175091, 450919]]),
-            ],
-            [
-                max_value + 1000,
-                shapely.Polygon([[175012, 450920], [175011, 450907], [175019, 450906], [175012, 450920]]),
-            ],
-        ],
-        geometry="geometry",
-        crs=Config.CRS,
-        columns=["suitability_value", "geometry"],
-    )
-    # 2. group a - partial overlap
-    criterion_a_2 = gpd.GeoDataFrame(
-        data=[
-            # Overlaps criterion a 1 with a higher value
-            [50, shapely.Polygon([[174797, 451107], [174944, 451090], [174807, 451129], [174797, 451107]])],
-        ],
-        geometry="geometry",
-        crs=Config.CRS,
-        columns=["suitability_value", "geometry"],
-    )
-    # 3. group b - partial overlap
-    criterion_b_1 = gpd.GeoDataFrame(
-        data=[
-            # Overlaps criterion a 1 with a higher value
-            [20, shapely.Point([174813.28, 451113.88])],
-            [1000, shapely.Point([174870.46, 451051.07])],
-            [-20, shapely.Point([175013.310, 450910.294])],
-            [-1, shapely.Polygon([[175087, 450911], [175107, 450912], [175087, 450915], [175087, 450911]])],
-            # Overlaps criterion a 1 with the same value but signed.
-            [-5, shapely.Polygon([[174830, 451074], [174842, 451069], [174831, 451061], [174830, 451074]])],
-        ],
-        geometry="geometry",
-        crs=Config.CRS,
-        columns=["suitability_value", "geometry"],
-    )
-    # 4. group b - overlaps criterion b1 and a1
-    criterion_b_2 = gpd.GeoDataFrame(
-        data=[
-            [15, shapely.Polygon([[175096, 450908], [175089, 450908], [175091, 450921], [175096, 450908]])],
-        ],
-        geometry="geometry",
-        crs=Config.CRS,
-        columns=["suitability_value", "geometry"],
-    )
-    # 5. group b - tree on the edge of the project area. The buffer exceeds the project area boundary.
-    criterion_b_3 = gpd.GeoDataFrame(
-        data=[[10, shapely.Point(174741.950, 451113.084).buffer(10)]],
-        geometry="geometry",
-        crs=Config.CRS,
-        columns=["suitability_value", "geometry"],
-    )
-    # 6. group c - overlapping a1
-    criterion_c_1 = gpd.GeoDataFrame(
-        data=[
-            [1, shapely.Polygon([[174729, 451158], [174940, 451115], [174841, 451195], [174729, 451158]])],
-            [10, shapely.Polygon([[174915, 451128], [174924, 451135], [174926, 451109], [174915, 451128]])],
-        ],
-        geometry="geometry",
-        crs=Config.CRS,
-        columns=["suitability_value", "geometry"],
-    )
-    # 7. group c - overlapping b1 and c1
-    criterion_c_2 = gpd.GeoDataFrame(
-        data=[
-            [1, shapely.Polygon([[175090, 450906], [175103, 450905], [175096, 450918], [175090, 450906]])],
-            [39, shapely.Polygon([[174811, 451226], [174834, 451155], [174909, 451174], [174811, 451226]])],
-        ],
-        geometry="geometry",
-        crs=Config.CRS,
-        columns=["suitability_value", "geometry"],
-    )
+    all_criteria_vectors: list[tuple[str, str, gpd.GeoDataFrame]] = multi_criteria_vectors(max_value, min_value)
+
     points_to_sample = gpd.GeoDataFrame(
         data=[
             [1, 14, shapely.Point(175090.35, 450911.67)],  # overlap between b1 and b2
@@ -309,13 +227,9 @@ def test_sum_rasters(debug=False):
     )
     if debug:
         # Using QGIS, it is easier doublecheck what values we are expecting in this test.
-        criterion_a_1.to_file(Config.PATH_RESULTS / "pytest_sum_criterion_a1.geojson")
-        criterion_a_2.to_file(Config.PATH_RESULTS / "pytest_sum_criterion_a2.geojson")
-        criterion_b_1.to_file(Config.PATH_RESULTS / "pytest_sum_criterion_b1.geojson")
-        criterion_b_2.to_file(Config.PATH_RESULTS / "pytest_sum_criterion_b2.geojson")
-        criterion_b_3.to_file(Config.PATH_RESULTS / "pytest_sum_criterion_b3.geojson")
-        criterion_c_1.to_file(Config.PATH_RESULTS / "pytest_sum_criterion_c1.geojson")
-        criterion_c_2.to_file(Config.PATH_RESULTS / "pytest_sum_criterion_c2.geojson")
+        for criterion in all_criteria_vectors:
+            criterion_name, _, criterion_gdf = criterion
+            criterion_gdf.to_file(Config.PATH_RESULTS / f"pytest_sum_{criterion_name}.geojson")
         points_to_sample.to_file(Config.PATH_RESULTS / "pytest_sum_points_to_sample.geojson")
 
     rasters_to_merge = []
@@ -324,18 +238,10 @@ def test_sum_rasters(debug=False):
     )
     raster_settings = get_raster_settings(project_area, 0.5)
     for (
+        criterion_name,
         group,
         criterion_gdf,
-        criterion_name,
-    ) in [
-        ["a", criterion_a_1, "criterion_a1"],
-        ["a", criterion_a_2, "criterion_a2"],
-        ["b", criterion_b_1, "criterion_b1"],
-        ["b", criterion_b_2, "criterion_b2"],
-        ["b", criterion_b_3, "criterion_b3"],
-        ["c", criterion_c_1, "criterion_c1"],
-        ["c", criterion_c_2, "criterion_c2"],
-    ]:
+    ) in all_criteria_vectors:
         rasterized_vector = rasterize_vector_data(criterion_name, criterion_gdf, raster_settings)
         rasters_to_merge.append(RasterizedCriterion(criterion_name, rasterized_vector, group))
 
